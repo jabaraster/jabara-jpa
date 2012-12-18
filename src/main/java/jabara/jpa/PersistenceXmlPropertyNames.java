9eb724641607c2bcd65a8659892e7d8bfcde9cd2
Jabara <jabaraster@gmail.com>
2012-12-18 13:16:50 +0900

    add 'PersistenceXmlPropertyNames'

---
+++ src/main/java/jabara/jpa/PersistenceXmlPropertyNames.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package jabara.jpa;
+
+/**
+ * @author jabaraster
+ */
+public final class PersistenceXmlPropertyNames {
+
+    /**
+     * 
+     */
+    public static final String DRIVER        = "javax.persistence.driver";       //$NON-NLS-1$
+    /**
+     * 
+     */
+    public static final String JDBC_URL      = "javax.persistence.jdbc.url";     //$NON-NLS-1$
+    /**
+     * 
+     */
+    public static final String JDBC_USER     = "javax.persistence.jdbc.user";    //$NON-NLS-1$
+    /**
+     * 
+     */
+    public static final String JDBC_PASSWORD = "javax.persistence.jdbc.password"; //$NON-NLS-1$
+
+    private PersistenceXmlPropertyNames() {
+        // 処理なし
+    }
+}
