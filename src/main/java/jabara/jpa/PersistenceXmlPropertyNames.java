12e20bce3c46930f228ccdc2d0d6b9906b052a56
jabaraster <jabaraster@gmail.com>
2014-08-02 18:02:21 +0900

    mainte.

--- src/main/java/jabara/jpa/PersistenceXmlPropertyNames.java
+++ src/main/java/jabara/jpa/PersistenceXmlPropertyNames.java
@@ -35,7 +35,7 @@ public final class PersistenceXmlPropertyNames {
     public static final class Hibernate {
 
         /**
-         * JPAエンティティの定義に従ってDBのスキーマを作り替える場合、persistence.xmlにこの名前のプロパティを定義します. <br>
+         * hibernate-entitymanagerを使っているときに、JPAエンティティの定義に従ってDBのスキーマを作り替える場合、persistence.xmlにこの名前のプロパティを定義します. <br>
          * 値はcreate-drop、createなどを指定します. <br>
          */
         public static final String HBM2DDL_AUTO                   = "hibernate.hbm2ddl.auto"; //$NON-NLS-1$
@@ -43,6 +43,18 @@ public final class PersistenceXmlPropertyNames {
          * JPAエンティティの定義に従ってDBのスキーマを作り替える場合に指定するプロパティの値です. <br>
          */
         public static final String HBM2DDL_AUTO_VALUE_CREATE_DROP = "create-drop";           //$NON-NLS-1$
+        /**
+         * JPAエンティティの定義に従ってDBのスキーマを作り替える場合に指定するプロパティの値です. <br>
+         */
+        public static final String HBM2DDL_AUTO_VALUE_CRAETE      = "create";                //$NON-NLS-1$
+        /**
+         * JPAエンティティの定義に従ってDBのスキーマを作り替える場合に指定するプロパティの値です. <br>
+         */
+        public static final String HBM2DDL_AUTO_VALUE_NONE        = "none";                  //$NON-NLS-1$
+        /**
+         * hibernate-entitymanagerを使っているときに、persistence.xmlにこのプロパティ名で方言(Dialect)クラスを指定します. <br>
+         */
+        public static final String DIALECT                        = "hibernate.dialect";     //$NON-NLS-1$
 
         private Hibernate() {
             // 処理なし
